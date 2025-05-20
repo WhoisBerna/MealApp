de5c235039ce6353a058fa349c0ad56ddc7756b7
Bernardo Augusto <beraugustoalmeida2016@gmail.com>
2025-05-20 19:20:24 -0300

    feat: styling the navbar with tailwind

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
+import { SignedIn, SignedOut, useUser, SignOutButton } from "@clerk/nextjs";
 
 export default function NavBar() {
     const { isLoaded, isSignedIn, user } = useUser();
@@ -10,23 +10,59 @@ export default function NavBar() {
     if (!isLoaded) return <p>Loading... </p>
 
     return (
-        <nav>
-            <div>
+        <nav className="fixed top-0 left-0 w-full bg-white shadow-sm z-50">
+            <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
                 <Link href={"/"}>
-                    <Image src="/logo.png" width={80} height={80} alt="Logo" />
+                    <Image
+                        className="text-xl font-bold text-emerald-700 cursor-pointer"
+                        src="/logo.png"
+                        width={80}
+                        height={80}
+                        alt="Logo" />
                 </Link>
-            </div>
-            <div>
-                <SignedIn>
-                    <Link href={"/mealplan"}> MealPlan! </Link>
-                    {user?.imageUrl ?
-                     (<Link href="/profile"> <Image src={user.imageUrl} height={60} width={60} alt="Profile Logo" />
-                    </Link>) 
-                    : <div> Image Not Found </div>}
-                </SignedIn>
-                <SignedOut>
-                    <Link href={"/sign-up"}> Sign Up! </Link>
-                </SignedOut>
+                <div className="space-x-6 flex items-center">
+                    <SignedIn>
+                        <Link
+                            className="text-black-700 hover:text-red-500 transition-colors"
+                            href={"/mealplan"}>
+                            MealPlan!
+                        </Link>
+                        {user?.imageUrl ? (
+                            <Link href="/profile">
+                                <Image
+                                    className="rounded-full"
+                                    src={user.imageUrl}
+                                    height={60}
+                                    width={60}
+                                    alt="Profile Logo" />
+                            </Link>)
+                            : (
+                                <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
+                            )}
+                        <SignOutButton>
+                            <button className="al-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition">
+                                Sign Out
+                            </button>
+                        </SignOutButton>
+                    </SignedIn>
+                    <SignedOut>
+                        <Link 
+                        className="text-black-800 hover:text-red-600 transition-colors"
+                        href={"/"}>
+                            Home
+                        </Link>
+                        <Link 
+                        className="text-black-800 hover:text-red-600 transition-colors"
+                        href={isSignedIn ? "/subscribe" : "/sign-up"}>
+                            Subscribe
+                        </Link>
+                        <Link 
+                        className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
+                        href={"/sign-up"}>
+                            Sign Up!
+                        </Link>
+                    </SignedOut>
+                </div>
             </div>
         </nav>
     )
